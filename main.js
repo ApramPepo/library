da85f1e30d472b3c8013ab77158a3f526d421d60
ApramPepo <120373555+ApramPepo@users.noreply.github.com>
2025-05-01 00:26:21 +0300

    Final project -debugging

--- main.js
+++ main.js
@@ -21,14 +21,45 @@ function createBook(title, author, page, isRead, Year) {
 
 function addBookToLibrary(book) {
     myLibrary.push(book);
+    displayBooks();
 }
 
 function displayBooks() {
+    const libraryDiv = document.getElementById('library');
+    libraryDiv.innerHTML = '';
+
     myLibrary.forEach(
         (book, index) => {
             const card = document.createElement('div');
-            card.textContent = `${book.title} by ${book.author}, ${book.page}, ${book.Year}, Read: ${book.isRead}`;
-            document.body.appendChild(card);
+            card.className = 'card';
+            card.innerHTML = `
+            <h2>${book.title} by <i>${book.author}</i></h2>
+            <p>${book.page} Pages</p>
+            <p>Released in ${book.Year}</p>
+            <p>Read? <strong>${book.isRead ? 'Yes' : 'No'}</strong></p>
+            <button onclick="toggleRead(${index})">Toggle Read</button>
+            <button onclick="removeBook(${index})">Remove</button>
+            `;
+            libraryDiv.appendChild(card);
         }
     );
 }
+
+function removeBook(index) {
+    myLibrary.splice(index, 1);
+    displayBooks();
+}
+
+const bookShelf = document.querySelector('.bookShelf')
+
+bookShelf.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const title = document.getElementById('title').value;
+    const author = document.getElementById('author').value;
+    const pages = document.getElementById('page').value;
+    const year = document.getElementById('Year').value;
+    const isRead = document.getElementById('isRead').value;
+
+    const newBook = createBook(title, author, pages, isRead, year);
+    addBookToLibrary(newBook);
+});
